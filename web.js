9455695cdce7ae586a60b715b745cc211a656a5e
Saul Lugo <saullugolugo@gmail.com>
2013-07-08 16:36:15 +0000

    Testing read file and buffer in web.js

--- web.js
+++ web.js
@@ -1,6 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
+var indexstr = buf.toString(fs.readFileSync(index.html));
+
+console.log("My string is: " + indexstr + "\n");
+
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
@@ -10,3 +14,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
