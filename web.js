83a2a8e1fd8f9a89d3ce01652c2dccfcbbf8bfc4
Saul Lugo <saullugolugo@gmail.com>
2013-07-08 19:08:15 +0000

    Now web.js reads the response from index.html

--- web.js
+++ web.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-var indexstr = buf.toString(fs.readFileSync(index.html));
+var buf = fs.readFileSync("index.html");
+var mystr = buf.toString();
 
-console.log("My string is: " + indexstr + "\n");
+console.log("My string is: " + mystr + "\n");
 
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(mystr);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
